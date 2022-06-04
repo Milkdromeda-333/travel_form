72cc5d2fbe27954bbf81099a67550fdbc935f11b
Anjanique M <99520247+Milkdromeda-333@users.noreply.github.com>
2022-06-04 01:03:01 -0700

    Update app.js
    Finished JS

--- app.js
+++ app.js
@@ -5,13 +5,21 @@ document.addEventListener("submit", function (e) {
     const firstName = myForm.firstName.value;
     const lastName = myForm.lastName.value;
     const age = myForm.age.value;
+
     let genderOptions = myForm.gender;
     let gender;
     for (let i = 0; i < genderOptions.length; i++) {
         if (genderOptions[i].checked) {
             gender = genderOptions[i].value;
-        }
+        };
     }
+
+    let select = myForm.getElementsByTagName("select")[0];
+    const destinationIndex = select.selectedIndex;
+    const destination = select.options[destinationIndex].value;
+
+    alert(`First Name: ${firstName}\nLast Name: ${lastName}\nAge: ${age}\nGender: ${gender}\nDestination: ${destination}`);
+
 
 
 });
